9b857c462eb0a16182b691ee433d1855493be1ed
DM <dm@arillo.net>
2016-10-13 23:59:05 +0200

    set locale from session if fluent is installed

--- code/DataObjectPreviewController.php
+++ code/DataObjectPreviewController.php
@@ -9,8 +9,9 @@ class DataObjectPreviewController extends Controller {
 
 	public function show($request){
 		$class = $request->param('OtherID');
-		// @TODO: check if using fluent, set locale to current CMS locale
-		// Fluent::set_persist_locale(Session::get('FluentLocale_CMS'));
+        if (class_exists('Fluent')) {
+		  Fluent::set_persist_locale(Session::get('FluentLocale_CMS'));
+        }
 		$this->dataobject = $class::get()->filter(array('ID' => $request->param('ID')))->First();
 		if ( is_null($this->dataobject) ) {
 			return $this->customise(array(
