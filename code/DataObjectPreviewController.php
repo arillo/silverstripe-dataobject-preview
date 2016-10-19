4e6696d4672e3cc18e6081078775eec9b8b33d60
DM <dm@arillo.net>
2016-10-19 12:51:00 +0200

    fix template name

--- code/DataObjectPreviewController.php
+++ code/DataObjectPreviewController.php
@@ -1,26 +1,26 @@
 <?php
 class DataObjectPreviewController extends Controller {
 
-	public $dataobject;
+    public $dataobject;
 
-	public static $allowed_actions = array(
-		'show'
-		);
+    public static $allowed_actions = array(
+        'show'
+    );
 
-	public function show($request){
-		$class = $request->param('OtherID');
+    public function show($request){
+        $class = $request->param('OtherID');
         if (class_exists('Fluent')) {
-		  Fluent::set_persist_locale(Session::get('FluentLocale_CMS'));
+            Fluent::set_persist_locale(Session::get('FluentLocale_CMS'));
         }
-		$this->dataobject = $class::get()->filter(array('ID' => $request->param('ID')))->First();
-		if ( is_null($this->dataobject) ) {
-			return $this->customise(array(
-				'Layout' => $this->renderWith('DataObjectNotFound')
-				))->renderWith('PreviewDataObject');
-		}
-		return $this->customise(array(
-			'Layout' => $this->dataobject->renderWith($class)
-			))->renderWith('PreviewDataObject');
-	}
+        $this->dataobject = $class::get()->filter(array('ID' => $request->param('ID')))->First();
+        if ( is_null($this->dataobject) ) {
+            return $this->customise(array(
+                'Layout' => $this->renderWith('DataObjectPreviewNotFound')
+                ))->renderWith('PreviewDataObject');
+        }
+        return $this->customise(array(
+            'Layout' => $this->dataobject->renderWith($class)
+            ))->renderWith('PreviewDataObject');
+    }
 
 }
