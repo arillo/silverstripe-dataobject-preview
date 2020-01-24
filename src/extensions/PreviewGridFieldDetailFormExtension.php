a994a41807f84c70e6ecc04532a9114d46bc378e
banglashi <dm@arillo.net>
2020-01-24 01:06:27 +0100

    cleanup

--- src/extensions/PreviewGridFieldDetailFormExtension.php
+++ src/extensions/PreviewGridFieldDetailFormExtension.php
@@ -7,7 +7,6 @@ use SilverStripe\Core\Extension;
 use SilverStripe\CMS\Controllers\SilverStripeNavigator;
 use SilverStripe\ORM\CMSPreviewable;
 use SilverStripe\Forms\LiteralField;
-use SilverStripe\View\Requirements;
 
 class PreviewGridFieldDetailFormExtension extends Extension
 {
@@ -16,24 +15,15 @@ class PreviewGridFieldDetailFormExtension extends Extension
         $fields = $form->Fields();
         if ($this->owner->record instanceof CMSPreviewable && !$fields->fieldByName('SilverStripeNavigator'))
         {
-            $this->injectNavigatorAndPreview($form, $fields);
+            $fields->removeByName('SilverStripeNavigator');
+            $ctrl = Controller::curr();
+            $navigator = new SilverStripeNavigator($this->owner->record);
+            $field = new LiteralField('SilverStripeNavigator', $navigator->renderWith($ctrl->getTemplatesWithSuffix('_SilverStripeNavigator')));
+            $field->setAllowHTML(true);
+            $fields->push($field);
+            $form->addExtraClass('cms-previewable');
+            $form->addExtraClass('cms-previewabledataobject');
+            $form->removeExtraClass('cms-panel-padded center');
         }
-    }
-
-    private function injectNavigatorAndPreview(&$form, &$fields)
-    {
-        // Debug::dump(DATAOBJECT_PREVIEW_DIR.'/javascript/GridField.Preview.js');
-        // die();
-        // Requirements::javascript(DATAOBJECT_PREVIEW_DIR.'/javascript/GridField.Preview.js');
-        Requirements::javascript('arillo/silverstripe-dataobject-preview:client/javascript/GridField.Preview.js');
-        //@TODO: Do we need to verify we are in the right controller?
-        $template = Controller::curr()->getTemplatesWithSuffix('_SilverStripeNavigator');
-        $navigator = new SilverStripeNavigator($this->owner->record);
-        $field = new LiteralField('SilverStripeNavigator', $navigator->renderWith($template));
-        $field->setAllowHTML(true);
-        $fields->push($field);
-        $form->addExtraClass('cms-previewable');
-        $form->addExtraClass('cms-previewabledataobject');
-        $form->removeExtraClass('cms-panel-padded center');
     }
 }
