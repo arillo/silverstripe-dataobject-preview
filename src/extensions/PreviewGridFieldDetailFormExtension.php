18d0fbdd5a2cb376cf524244ab6014877d86ab18
banglashi <dm@arillo.net>
2020-01-30 16:14:16 +0100

    remove Navigator from ModelAdmin instances

--- src/extensions/PreviewGridFieldDetailFormExtension.php
+++ src/extensions/PreviewGridFieldDetailFormExtension.php
@@ -2,6 +2,7 @@
 
 namespace SilverStripe\DataObjectPreview\Extensions;
 
+use SilverStripe\Admin\ModelAdmin;
 use SilverStripe\Control\Controller;
 use SilverStripe\Core\Extension;
 use SilverStripe\CMS\Controllers\SilverStripeNavigator;
@@ -17,13 +18,15 @@ class PreviewGridFieldDetailFormExtension extends Extension
         {
             $fields->removeByName('SilverStripeNavigator');
             $ctrl = Controller::curr();
-            $navigator = new SilverStripeNavigator($this->owner->record);
-            $field = new LiteralField('SilverStripeNavigator', $navigator->renderWith($ctrl->getTemplatesWithSuffix('_SilverStripeNavigator')));
-            $field->setAllowHTML(true);
-            $fields->push($field);
-            $form->addExtraClass('cms-previewable');
-            $form->addExtraClass('cms-previewabledataobject');
-            $form->removeExtraClass('cms-panel-padded center');
+            if(!$ctrl instanceof ModelAdmin){
+                $navigator = new SilverStripeNavigator($this->owner->record);
+                $field = new LiteralField('SilverStripeNavigator', $navigator->renderWith($ctrl->getTemplatesWithSuffix('_SilverStripeNavigator')));
+                $field->setAllowHTML(true);
+                $fields->push($field);
+                $form->addExtraClass('cms-previewable');
+                $form->addExtraClass('cms-previewabledataobject');
+                $form->removeExtraClass('cms-panel-padded center');
+            }
         }
     }
 }
