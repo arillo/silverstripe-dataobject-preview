a638f7fd3d903d410ae94525010e16b5650e91d0
dm <dm@arillo.net>
2021-05-12 23:46:45 +0200

    revert back

--- src/extensions/PreviewGridFieldDetailFormExtension.php
+++ src/extensions/PreviewGridFieldDetailFormExtension.php
@@ -2,7 +2,6 @@
 
 namespace SilverStripe\DataObjectPreview\Extensions;
 
-use SilverStripe\Admin\ModelAdmin;
 use SilverStripe\Control\Controller;
 use SilverStripe\Core\Extension;
 use SilverStripe\CMS\Controllers\SilverStripeNavigator;
@@ -16,17 +15,21 @@ class PreviewGridFieldDetailFormExtension extends Extension
     {
         $fields = $form->Fields();
         if ($this->owner->record instanceof CMSPreviewable && !$fields->fieldByName('SilverStripeNavigator')) {
-            $ctrl = Controller::curr();
-            if ($ctrl instanceof ModelAdmin) {
-                Requirements::javascript('arillo/silverstripe-dataobject-preview:client/javascript/GridField.Preview.js');
-                $navigator = new SilverStripeNavigator($this->owner->record);
-                $field = new LiteralField('SilverStripeNavigator', $navigator->renderWith($ctrl->getTemplatesWithSuffix('_SilverStripeNavigator')));
-                $field->setAllowHTML(true);
-                $fields->push($field);
-                $form->addExtraClass('cms-previewable');
-                $form->addExtraClass('cms-previewabledataobject');
-                $form->removeExtraClass('cms-panel-padded center');
-            }
+            $this->injectNavigatorAndPreview($form, $fields);
         }
+    }
+
+    private function injectNavigatorAndPreview(&$form, &$fields)
+    {
+        Requirements::javascript('arillo/silverstripe-dataobject-preview:client/javascript/GridField.Preview.js');
+        //@TODO: Do we need to verify we are in the right controller?
+        $template = Controller::curr()->getTemplatesWithSuffix('_SilverStripeNavigator');
+        $navigator = new SilverStripeNavigator($this->owner->record);
+        $field = new LiteralField('SilverStripeNavigator', $navigator->renderWith($template));
+        $field->setAllowHTML(true);
+        $fields->push($field);
+        $form->addExtraClass('cms-previewable');
+        $form->addExtraClass('cms-previewabledataobject');
+        $form->removeExtraClass('cms-panel-padded center');
     }
 }
