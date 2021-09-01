65b2c45684486a0e14813560ccec4f4583ac874f
Jay Richardson <jay@werkbot.com>
2021-09-01 13:58:18 -0400

    Fix for switching between edit/preview/split modes

---
+++ src/extensions/PreviewGridFieldDetailForm_ItemRequest.php
@@ -0,0 +1,26 @@
+<?php
+/**/
+namespace SilverStripe\DataObjectPreview\Extensions;
+/**/
+use SilverStripe\Forms\LiteralField;
+use SilverStripe\Forms\GridField\GridFieldDetailForm_ItemRequest;
+/**/
+class PreviewGridFieldDetailForm_ItemRequest extends GridFieldDetailForm_ItemRequest{
+  /**/
+  public function ItemEditForm(){
+
+    $form = parent::ItemEditForm();
+    $formActions = $form->Actions();
+
+    $html = $this->customise([
+      "SelectID" => "preview-mode-dropdown-in-content"
+    ])->renderWith(array('SilverStripe\\Admin\\LeftAndMain_ViewModeSelector'));
+
+    $PreviewLink = LiteralField::create('', $html);
+    $formActions->push($PreviewLink);
+
+    $form->setActions($formActions);
+
+    return $form;
+  }
+}
